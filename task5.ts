93a0dd3de4f3e22fe0e187895273ad9c456bfbd7
Karina <karyna.karmaza@gmail.com>
2026-02-08 22:17:50 +0200

    feat: add solution for task5

--- task5.ts
+++ task5.ts
@@ -1,6 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая разворачивает вложенные массивы в один
 
 const array = [1, [2, 3], [4], 5, [6, 7, 8]];
 
-const result = [1, 2, 3, 4, 5, 6, 7, 8];
+//const result = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const unpackArray = (arr: (number | number[])[]) => {
+  const result: number[] = [];
+  for (const value of arr) {
+    if (Array.isArray(value)) {
+      for (const subvalue of value) {
+        result.push(subvalue);
+      }
+    } else {
+      result.push(value);
+    }
+  }
+  return result;
+};
+
+console.log(unpackArray(array));
